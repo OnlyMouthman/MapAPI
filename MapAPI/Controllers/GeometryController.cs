988987cbce1d2683948c79970bf6f722618dbb13
DESKTOP-IGT9E3N\User <onlymouthman@gmail.com>
2024-07-19 14:28:23 +0800

    1. GeometryController的SetPolygon測試(建置測試資料)

--- MapAPI/Controllers/GeometryController.cs
+++ MapAPI/Controllers/GeometryController.cs
@@ -5,6 +5,7 @@ using System.Text.Json.Serialization;
 using System.Text.Json;
 using MapAPI.Models.Test;
 using MapAPI.Models.Controller;
+using System.Xml.Linq;
 
 namespace MapAPI.Controllers
 {
@@ -71,6 +72,45 @@ namespace MapAPI.Controllers
             return Ok("OK");
         }
 
+        [HttpGet("SetPolygon")]
+        public ActionResult Get()
+        {
+            var gf = NetTopologySuite.NtsGeometryServices.Instance.CreateGeometryFactory(4326);
+            var ply1 = gf.CreatePolygon(new[] {
+                new NetTopologySuite.Geometries.Coordinate(23.000211702386608, 120.20001226250344),
+                new NetTopologySuite.Geometries.Coordinate(22.997751705006976, 120.19900963877257),
+                new NetTopologySuite.Geometries.Coordinate(22.99500495947909, 120.1978589196683),
+                new NetTopologySuite.Geometries.Coordinate(22.99486063548646, 120.19780779510972),
+                new NetTopologySuite.Geometries.Coordinate(22.99432726176107, 120.19767146226121),
+                new NetTopologySuite.Geometries.Coordinate(22.993091083447638,  120.19737834758234),
+                new NetTopologySuite.Geometries.Coordinate(22.991468974815888, 120.19698980052505),
+                new NetTopologySuite.Geometries.Coordinate(22.990753609563356, 120.19681256871979),
+                new NetTopologySuite.Geometries.Coordinate(22.990747334412262, 120.19688073479847),
+                new NetTopologySuite.Geometries.Coordinate(22.991447012830434,  120.19706137466261),
+                new NetTopologySuite.Geometries.Coordinate(22.993069507399866, 120.19744444559944),
+                new NetTopologySuite.Geometries.Coordinate(22.99483278184036, 120.19786366646343),
+                new NetTopologySuite.Geometries.Coordinate(22.994964555953587, 120.19792160763137),
+                new NetTopologySuite.Geometries.Coordinate(22.99645850099445,  120.19853907453938),
+                new NetTopologySuite.Geometries.Coordinate(22.99781614335143, 120.19912515007064),
+                new NetTopologySuite.Geometries.Coordinate(22.9988237294083, 120.19953958743855),
+                new NetTopologySuite.Geometries.Coordinate(23.000181245452367, 120.20010679751817),
+                new NetTopologySuite.Geometries.Coordinate(23.000211702386608, 120.20001226250344),
+            });
 
+            DateTime? date1 = new DateTime(2024, 7, 18);
+            GeoData data = new GeoData
+            {
+                Tag = 1,
+                Geo = ply1,
+                Chinese = "",
+                English = "",
+                Date = date1,
+                Describe = ""
+            };
+            _context.GeoData.Add(data);
+            _context.SaveChanges();
+
+            return Ok("OK");
+        }
     }
 }
--- MapAPI/Models/Controller/Geometry.cs
+++ MapAPI/Models/Controller/Geometry.cs
@@ -22,4 +22,21 @@ namespace MapAPI.Models.Controller
 
         public int Tag { get; set; }
     }
+
+    public partial class MyPolygon
+    {
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string Chinese { get; set; }
+
+        public string English { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Describe { get; set; }
+
+        public int Tag { get; set; }
+    }
 }
